c38bc160ff44b9e50343828ddfcf7307c27fc4fe
Kaushal M <kshlmster@gmail.com>
2014-04-04 11:01:58 +0530

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 
-from setuptools import setup, find_packages
-import sys, os
+from setuptools import setup
 
 setup(name='gant',
     version='0.0.1',
     description="The Gluster development helper ant",
     long_description="The Gluster development helper ant, gant for short, helps developer of GlusterFS by createing a standardized development and testing environment using Docker.",
-    classifiers=[],
-    keywords='',
+    keywords='gluster docker utility development testing',
     author='Kaushal M',
     author_email='kshlmster@gmail.com',
     url='http://github.com/kshlm/gant',
     license='BSD',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    include_package_data=True,
-    zip_safe=False,
-    test_suite='nose.collector',
+    classifiers=[
+        "Development Status :: 3 - Pre-Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        "Environment :: Console",
+    ],
+    packages=('gant'),
     install_requires=[
         "docker-py",
         "aaargh",
         ],
-    setup_requires=[],
     entry_points="""
+        [console_scripts]
+        gant = gant.main:main
     """,
-    namespace_packages=[],
     )
