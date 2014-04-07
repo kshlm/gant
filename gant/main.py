b5e85a23019afdd13882501b30fe7aa1a887c523
Kaushal M <kshlmster@gmail.com>
2014-04-07 16:54:33 +0530

    Add permission checker and improve options

--- gant/main.py
+++ gant/main.py
@@ -2,6 +2,7 @@
 
 from docopt import docopt
 from .utils.gant_docker import GantDocker
+import os
 
 helpStr = """
 Gant : The Gluster helper ant
@@ -9,7 +10,7 @@ Creates GlusterFS development and testing environments using Docker
 
 Usage:
     gant [options] build-base [force]
-    gant [options] build-main [force]
+    gant [options] build-main <glusterfssrc>[force]
     gant [options] launch <number>
     gant [options] stop [<name>] [force]
     gant [options] info
@@ -17,21 +18,22 @@ Usage:
 
 Options:
     -c <conffile>, --conf <conffile>  Configuration file to use
-    --base-tag <basetag>              Tag to be used for the base docker image [default: glusterbase:latest]
-    --main-tag <maintag>              Tag to be used for the main docker image [default: gluster:latest]
+    --basetag <basetag>               Tag to be used for the base docker image [default: glusterbase:latest]
+    --maintag <maintag>               Tag to be used for the main docker image [default: gluster:latest]
+    --basedir <basedir>               Base directory containing the Dockerfile and helper scripts for Gant [default: {0}]
     --prefix <prefix>                 Prefix to be used for naming the launched docker containers [default: gluster]
 
 Arguments:
     force   Forcefully do the operation
     number  Number of containers to launch
     name    Name of container to stop
-"""
-
+""".format(os.getcwd())
 
 def main():
     args = docopt(helpStr, version = "Gant v0.0.1")
     g = GantDocker()
 
+    print args
     if args["build-base"]:
         g.build_base_image (args)
     elif args["build-main"]:
--- gant/utils/gant_docker.py
+++ gant/utils/gant_docker.py
@@ -1,4 +1,18 @@
+import os
+import grp
 from docker_helper import DockerHelper
+
+def check_permissions():
+    """
+    Checks if current user can access docker
+    """
+    if not grp.getgrnam('docker').gr_gid in os.getgroups() and not os.geteuid() == 0:
+        exitStr = """
+        User doesn't have permission to use docker. You can do either of the following,
+        1. Add user to the 'docker' group (preferred)
+        2. Run command as superuser using either 'sudo' or 'su -c'
+        """
+        exit (exitStr)
 
 class GantDocker (DockerHelper):
     """
@@ -11,28 +25,50 @@ class GantDocker (DockerHelper):
         """
         Build the glusterbase image
         """
-        if self.image_exists(tag = BASEIMAGETAG):
-            if not args["force"]:
-                print "Image with tag '%s' already exists"%(BASEIMAGETAG)
-                return  self.image_by_tag (BASEIMAGETAG)
-            else:
-                self.remove_image (BASEIMAGETAG)
+        check_permissions()
 
-        self.build(path = BASEDIR, rm = True, tag = BASEIMAGETAG)
+        basetag = args["--basetag"]
+        bsaedir = args["--basedir"]
+        if self.image_exists(tag = basetag):
+            if not args["force"]:
+                print "Image with tag '%s' already exists"%(basetag)
+                return  self.image_by_tag (basetag)
+            else:
+                self.remove_image (basetag)
+
+        self.build(path = basedir, rm = True, tag = basedir)
 
     def build_main_image (self, args):
+        """
+        Build the main image to be used for launching containers
+        """
+        check_permissions()
+
         print "Would build the main image"
 
     def launch (self, args):
+        """
+        Launch the specified docker containers using the main image
+        """
+        check_permissions()
+
         print "Would launch %s containers using the main image"%(args["number"])
 
     def stop (self, args):
+        """
+        Stop the specified or all docker containers launched by us
+        """
+        check_permissions()
+
         if args["name"]:
             print "Would stop container %s"%(args["name"])
         else:
             print "Would stop all containers"
 
     def info (self, args):
+        """
+        Print information on the built up environment
+        """
         print 'Would print info on the gluster env'
 
     def ssh (self, args):
