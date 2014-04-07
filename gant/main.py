7ae979a3b9400021f25fd4761ae45e6c09fcddc6
Kaushal M <kshlmster@gmail.com>
2014-04-07 15:41:35 +0530

    Flesh out the cli

--- gant/main.py
+++ gant/main.py
@@ -17,11 +17,31 @@ Usage:
 
 Options:
     -c <conffile>, --conf <conffile>  Configuration file to use
-    force                             Forcefully do the operation
-    number                            Number of containers to launch
+    --base-tag <basetag>              Tag to be used for the base docker image [default: glusterbase:latest]
+    --main-tag <maintag>              Tag to be used for the main docker image [default: gluster:latest]
+    --prefix <prefix>                 Prefix to be used for naming the launched docker containers [default: gluster]
 
+Arguments:
+    force   Forcefully do the operation
+    number  Number of containers to launch
+    name    Name of container to stop
 """
 
 
 def main():
     args = docopt(helpStr, version = "Gant v0.0.1")
+    g = GantDocker()
+
+    if args["build-base"]:
+        g.build_base_image (args)
+    elif args["build-main"]:
+        g.build_main_image (args)
+    elif args["launch"]:
+        g.launch(args)
+    elif args["stop"]:
+        g.stop(args)
+    elif args["info"]:
+        g.info(args)
+    elif args["ssh"]:
+        g.ssh(args)
+
--- gant/utils/gant_docker.py
+++ gant/utils/gant_docker.py
@@ -7,14 +7,33 @@ class GantDocker (DockerHelper):
     def __init__ (self):
         super (GantDocker, self).__init__()
 
-    def build_base_image (self, force = False):
+    def build_base_image (self, args):
         """
         Build the glusterbase image
         """
         if self.image_exists(tag = BASEIMAGETAG):
-            if not force:
+            if not args["force"]:
+                print "Image with tag '%s' already exists"%(BASEIMAGETAG)
                 return  self.image_by_tag (BASEIMAGETAG)
             else:
                 self.remove_image (BASEIMAGETAG)
 
         self.build(path = BASEDIR, rm = True, tag = BASEIMAGETAG)
+
+    def build_main_image (self, args):
+        print "Would build the main image"
+
+    def launch (self, args):
+        print "Would launch %s containers using the main image"%(args["number"])
+
+    def stop (self, args):
+        if args["name"]:
+            print "Would stop container %s"%(args["name"])
+        else:
+            print "Would stop all containers"
+
+    def info (self, args):
+        print 'Would print info on the gluster env'
+
+    def ssh (self, args):
+        print 'Would ssh into container %s'%(args['name'])
