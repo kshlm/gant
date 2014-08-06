dfc992d601dcd1d08225c8f3d95b17b37de7dc75
Kaushal M <kshlmster@gmail.com>
2014-08-06 14:41:54 +0530

    Bump version: 0.0.8 → 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gant',
-    version='0.0.8',
+    version='0.1.0',
     description="The Gluster development helper ant",
     long_description="The Gluster development helper ant, gant for short, helps in the development of GlusterFS by creating a standardized development and testing environment using Docker.",
     keywords='gluster docker utility development testing',
